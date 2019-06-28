9f94ef8d615ee9b7e1a2d608eafff096bb88e5d5
Jean Paul Gonneau <jgonneau@j-p.work>
2019-06-28 12:11:05 +0200

    init dev

--- src/generator.js
+++ src/generator.js
@@ -1,9 +1,7 @@
 function createBitcoin() {
-    console.log('Loading ...')
+    console.log('Loading ...');
 
     setTimeout(() => console.log('Created !'), 1000);
-
-    let varToRemove = true;
 }
 
 module.exports = {
